a788a0f93e968444107d222b46070b397a43094e
Uku Pattak <uku.pattak@remato.ee>
2021-04-10 19:27:19 +0300

    Improve dev env conf

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = {
-	development: ['last 2 versions', 'not IE 11', 'not dead'],
+	development: ['> 1%', 'last 2 versions', 'not IE 11', 'not dead'],
 	production: ['> 0.5%', 'last 3 major versions', 'not IE 11', 'not dead'],
 }
