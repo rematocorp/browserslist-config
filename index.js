e1c476ff8b5ded367a518e1994a904b2725c38cb
Uku Pattak <uku.pattak@remato.ee>
2021-04-10 17:48:19 +0300

    Support optional chaining

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = {
-	development: ['last 1 version', 'not IE 11', 'not dead'],
-	production: ['> 0.5%', 'last 3 versions', 'not IE 11', 'not dead'],
+	development: ['last 2 versions', 'not IE 11', 'not dead'],
+	production: ['> 0.5%', 'last 3 major versions', 'not IE 11', 'not dead'],
 }
