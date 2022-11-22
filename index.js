930fd403bc9dd0da013965b3c45c8bc9087ad8e8
samueleiche <samuel@samueleiche.com>
2022-11-22 15:35:33 +0200

    add staging and test env

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports = {
 	development: ['> 1%', 'last 2 versions', 'not IE 11', 'not dead'],
 	production: ['> 0.5%', 'last 4 years', 'not IE 11', 'not dead'],
+	staging: ['> 0.5%', 'last 4 years', 'not IE 11', 'not dead'],
+	test: ['> 0.5%', 'last 4 years', 'not IE 11', 'not dead'],
 }
